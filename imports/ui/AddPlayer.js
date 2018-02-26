5f2d0e9ffe96f392cdaeb71c907aa6bad3c89e3c
Charles Guertin <charlesguertin@live.ca>
2018-02-25 22:47:17 -0500

    App is now done.

--- imports/ui/AddPlayer.js
+++ imports/ui/AddPlayer.js
@@ -5,9 +5,8 @@ import PropTypes from 'prop-types';
 export default class AddPlayer extends React.Component {
   handleSubmit(e) {
     let playerName = e.target.playerName.value;
-
     e.preventDefault();
-
+    
     if (playerName) {
       e.target.playerName.value = '';
       Players.insert({
@@ -20,7 +19,7 @@ export default class AddPlayer extends React.Component {
     return (
       <div className="item">
         <form className="form" onSubmit={this.handleSubmit.bind(this)}>
-          <input className="form__input" type="text" name="playerName" placeholder="Player name"/>
+          <input required className="form__input" type="text" name="playerName" placeholder="Player name"/>
           <button className="button">Add Player</button>
         </form>
       </div>
